af8785f51e07dd7f531f52a6984f37aee6e76ba8
Mark Lavin <mlavin@caktusgroup.com>
2015-01-15 17:01:59 -0500

    Minimal login page.

---
+++ hygiene/static/hygiene/js/app.js
@@ -0,0 +1,16 @@
+var config = (function ($, Backbone, _) {
+
+    $(document).ready(function () {
+        var loginView = new config.views.LoginView();
+
+        loginView.on('login', function (token) {
+            $.ajaxPrefilter(function (settings, options, xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+            });
+            // TODO: Render the question
+        });
+    });
+
+    return JSON.parse($('#config').text());
+
+})(jQuery, Backbone, _);
---
+++ hygiene/static/hygiene/js/views.js
@@ -0,0 +1,31 @@
+(function ($, Backbone, _, config) {
+
+    config.views = {};
+
+    config.views.LoginView = Backbone.View.extend({
+        el: '#login',
+        events: {
+            'submit': 'submit'
+        },
+        submit: function (e) {
+            e.preventDefault();
+            var data = JSON.stringify({
+                username: $(':input[name="username"]', this.$el).val(),
+                password: $(':input[name="password"]', this.$el).val(),
+            });
+            $('.error', this.$el).remove();
+            $.post(this.$el.attr('action'), data)
+                .done($.proxy(this.login, this))
+                .fail($.proxy(this.fail, this));
+        },
+        login: function (result) {
+            this.trigger('login', result.token);
+            console.log(result);
+            this.$el.hide();
+        },
+        fail: function () {
+            this.$el.prepend('<p class="error">Invalid username/password</p>');
+        }
+    });
+
+})(jQuery, Backbone, _, config);
