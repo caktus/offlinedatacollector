95322068c82d2f63d2e62f476033b855d031d0d2
Mark Lavin <mlavin@caktusgroup.com>
2015-01-15 17:07:30 -0500

    Enable token auth in the settings.

--- offlinedatacollector/settings/base.py
+++ offlinedatacollector/settings/base.py
@@ -187,6 +187,7 @@ COMPRESS_PRECOMPILERS = (
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoObjectPermissions',
