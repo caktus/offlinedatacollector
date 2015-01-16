249c63668d2ad82a6b8a00f3b8883fc4241afbb8
David Ray <davidray@gmail.com>
2015-01-16 13:46:14 -0500

    replace bad hack that does not work anyway with temp dir creation and setting override

--- hygiene/tests/test_views.py
+++ hygiene/tests/test_views.py
@@ -1,12 +1,27 @@
+import shutil
+import tempfile
+
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.utils import override_settings
 
 
-@override_settings(COMPRESS_OUTPUT_DIR=settings.COMPRESS_ROOT)
-class CacheManifextTestCase(TestCase):
+class TempDirectoryMixin(object):
+    "Helper mixin for creating a temp directory and files inside."
+
+    def setUp(self):
+        super(TempDirectoryMixin, self).setUp()
+        self.temp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        super(TempDirectoryMixin, self).tearDown()
+        # Remove temp dir
+        shutil.rmtree(self.temp_dir, ignore_errors=True)
+
+
+class CacheManifextTestCase(TempDirectoryMixin, TestCase):
     def test_get_page(self):
         """Audit detail"""
         with self.assertTemplateUsed('hygiene/manifest.appcache'):
-            self.client.get(reverse('cache-manifest'))
+            with self.settings(LOGIN_URL=self.temp_dir):
+                self.client.get(reverse('cache-manifest'))
